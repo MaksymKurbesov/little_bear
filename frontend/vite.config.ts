dfeb6669954a2f1d8e3c82277a2cdd929b27d6ad
unknown <bonyklade@gmail.com>
2024-08-10 05:52:13 +0300

    fixed vite-plugin-deadfile bug

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,14 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import deadFile from "vite-plugin-deadfile";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    deadFile({
-      root: ".",
-      output: "deadfiles-report.json",
-    }),
-  ],
+  plugins: [react()],
 });
